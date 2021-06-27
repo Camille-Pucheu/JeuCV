69b2095977e980e15956e9afcc40a8856266b519
Camille Pucheu <camille.pucheu@gmail.com>
2021-06-27 19:52:13 +0200

    Complete html/css du Jeu des 5 différences

--- script/script.js
+++ script/script.js
@@ -1,16 +1,28 @@
 'use strict'
 
+
+/************************************************************************
+************************ Mes variables et Objets ************************
+************************************************************************/
+
 let compteur = 0;
-const textesPresentation = [
-    'Viens découvrir le CV de Camille au travers de petits jeux',
+let identifiantIntervalFlecheRouge;
+let mouvementFleche = 1;
+
+const textesIntroduction = [
+    'Viens découvrir le CV de Camille au travers d\'un petit jeu',
     'Mais si jamais le temps te manque...',
     'Tu peux accéder au CV de Camille en bas au gauche de l\'écran',
     ];
 
 const mesElementsCibles = {
-    boutonIconDePresentation : document.getElementById('presentation').children[1],
-    zoneTextDePresentation : document.getElementById('presentation').firstElementChild,
+    boutonIconDIntroduction : document.getElementById('introduction').children[1],
+    zoneTextDIntroduction : document.getElementById('introduction').firstElementChild,
 };
+
+/********************************************************************
+*************************** Mes Fonctions ***************************
+********************************************************************/
 
 
 const creationFlechRouge = function () {
@@ -22,8 +34,6 @@ const creationFlechRouge = function () {
     flecheRouge.style.bottom = "90px";
     document.getElementById('cv').appendChild(flecheRouge);
 };
-let identifiantIntervalFlecheRouge;
-let mouvementFleche = 1;
 let deplacementFleche = function() {
 	const leftFlecheRouge = parseFloat(document.getElementById('flecheRouge').style.left);
     const bottomFlecheRouge = parseFloat(document.getElementById('flecheRouge').style.bottom);
@@ -35,19 +45,22 @@ let deplacementFleche = function() {
 };
 
 
+/*******************************************************************
+************************** Mes Evènements **************************
+*******************************************************************/
 
-mesElementsCibles.boutonIconDePresentation.addEventListener('click',function(){
-    mesElementsCibles.zoneTextDePresentation.innerHTML = textesPresentation[compteur];
+
+/* Cet évenement gère la fenêtre d'introduction */
+mesElementsCibles.boutonIconDIntroduction.addEventListener('click',function(){
+    mesElementsCibles.zoneTextDIntroduction.innerHTML = textesIntroduction[compteur];
     compteur++;
     if (compteur == 3) {
-        mesElementsCibles.boutonIconDePresentation.src = "Images/Pikachu_Icon_Fermeture.png";
+        mesElementsCibles.boutonIconDIntroduction.src = "Images/Pikachu_Icon_Fermeture.png";
         creationFlechRouge();
         identifiantIntervalFlecheRouge = setInterval(deplacementFleche,8);
     } else if (compteur > 3) {
-        document.getElementById('presentation').style.display = 'none';
+        document.getElementById('introduction').style.display = 'none';
         document.getElementById('flecheRouge').style.display = 'none';
         clearInterval(identifiantIntervalFlecheRouge);
     };
 });
-
-
