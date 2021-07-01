d3acc5e7d2f85ccaa99e601046b3166422c7b8d7
Camille Pucheu <camille.pucheu@gmail.com>
2021-07-01 19:05:22 +0200

    Complete pokeball CV

---
+++ JavaScript/JS_script.js
@@ -0,0 +1,176 @@
+'use strict'
+
+
+/************************************************************************
+************************ Mes variables et Objets ************************
+************************************************************************/
+
+let compteur = 0;
+let identifiantIntervalFlecheRouge,identifiantIntervalFlecheBleue,identifiantIntervalFlecheVerte;
+let mouvementFleche = 1;
+
+const textesIntroduction = [
+    'Viens découvrir le CV de Camille au travers d\'un petit jeu',
+    'Mais si jamais le temps te manque...',
+    'Tu peux accéder au CV de Camille en bas au gauche de l\'écran',
+    'Sinon, tu pourras voir ton score en haut à droite',
+    'Qui te déverrouillera des parties du CV de Camille',
+    'À plus tard ! <3'
+    ];
+
+const textesJeux = [
+    'Ici, rien de plus simple, clique sur l\'anomalie trouvée.',
+    'Bravo, tu as tout trouvé!',
+    ];
+
+const mesElementsCibles = {
+    boutonIconDIntroduction : document.getElementById('introduction').children[1],
+    zoneTextDIntroduction : document.getElementById('introduction').firstElementChild,
+};
+
+/********************************************************************
+*************************** Mes Fonctions ***************************
+********************************************************************/
+
+
+const creationFlechRouge = function () {
+    var flecheRouge = document.createElement('img');
+    flecheRouge.id = 'flecheRouge';
+    flecheRouge.alt = "Flèche rouge pointant le lien du CV en format pdf";
+    flecheRouge.src = "Images/Flèche_Rouge.png";
+    flecheRouge.style.left = "80px";
+    flecheRouge.style.bottom = "90px";
+    document.getElementById('cv').appendChild(flecheRouge);
+};
+
+const creationFlechBleue = function () {
+    var flecheBleue = document.createElement('img');
+    flecheBleue.id = 'flecheBleue';
+    flecheBleue.alt = "Flèche bleue pointant le score";
+    flecheBleue.src = "Images/Flèche_Bleue.png";
+    flecheBleue.style.right = "155px";
+    flecheBleue.style.top = "75px";
+    document.getElementById('score').appendChild(flecheBleue);
+};
+
+const creationFlechVerte = function () {
+    var flecheVerte = document.createElement('img');
+    flecheVerte.id = 'flecheVerte';
+    flecheVerte.alt = "Flèche verte pointant les pokéballs";
+    flecheVerte.src = "Images/Flèche_Verte.png";
+    flecheVerte.style.right = "145px";
+    flecheVerte.style.bottom = "253px";
+    document.getElementById('pokeballCV').appendChild(flecheVerte);
+};
+
+const deplacementFlecheRouge = function() {
+	const leftFlecheRouge = parseFloat(document.getElementById('flecheRouge').style.left);
+    const bottomFlecheRouge = parseFloat(document.getElementById('flecheRouge').style.bottom);
+	if (leftFlecheRouge >= 130 || leftFlecheRouge < 80) {
+		mouvementFleche = -mouvementFleche;
+	};
+	document.getElementById('flecheRouge').style.left = leftFlecheRouge + mouvementFleche + "px";
+    document.getElementById('flecheRouge').style.bottom = bottomFlecheRouge + mouvementFleche + "px";
+};
+
+const deplacementFlecheBleue = function() {
+	const rightFlecheBleue = parseFloat(document.getElementById('flecheBleue').style.right);
+    const topFlecheBleue = parseFloat(document.getElementById('flecheBleue').style.top);
+	if (rightFlecheBleue >= 205 || rightFlecheBleue < 155) {
+		mouvementFleche = -mouvementFleche;
+	};
+	document.getElementById('flecheBleue').style.right = rightFlecheBleue + mouvementFleche + "px";
+    document.getElementById('flecheBleue').style.top = topFlecheBleue + mouvementFleche + "px";
+};
+
+const deplacementFlecheVerte = function() {
+	const rightFlecheVerte = parseFloat(document.getElementById('flecheVerte').style.right);
+    const bottomFlecheVerte = parseFloat(document.getElementById('flecheVerte').style.bottom);
+	if (rightFlecheVerte >= 190 || rightFlecheVerte < 145) {
+		mouvementFleche = -mouvementFleche;
+	};
+	document.getElementById('flecheVerte').style.right = rightFlecheVerte + mouvementFleche + "px";
+    document.getElementById('flecheVerte').style.bottom = bottomFlecheVerte + (mouvementFleche/3) + "px";
+};
+
+// creationFlechRouge();creationFlechBleue();creationFlechVerte();
+
+const diminutionOpacity = function(cible) {
+    let identifiant;
+    cible.style.opacity = 1;
+    identifiant = setInterval(function(){
+        let cibleOpacity = parseFloat(cible.style.opacity);
+        cible.style.opacity = cibleOpacity - 0.01;
+        if(cibleOpacity == 0) {
+            clearInterval(identifiant);
+            cible.style.display = 'none';
+        }
+    },10);
+}
+
+const augmentationOpacity = function (cible) {
+    let identifiant;
+    cible.style.display = 'block';
+    cible.style.opacity = 0;
+    identifiant = setInterval(function(){
+        let cibleOpacity = parseFloat(cible.style.opacity);
+        cible.style.opacity = cibleOpacity + 0.01;
+        if(cibleOpacity == 1) {
+            clearInterval(identifiant);
+        }
+    },10);
+}
+
+/*******************************************************************
+************************** Mes Evènements **************************
+*******************************************************************/
+
+
+/* Cet évenement gère la fenêtre d'introduction */
+mesElementsCibles.boutonIconDIntroduction.addEventListener('click',function(){
+    mesElementsCibles.zoneTextDIntroduction.innerHTML = textesIntroduction[compteur];
+    compteur++;
+    if (compteur == 3) {
+        creationFlechRouge();
+        identifiantIntervalFlecheRouge = setInterval(deplacementFlecheRouge,8);
+    } 
+    else if (compteur == 4) {
+        document.getElementById('flecheRouge').style.display = 'none';
+        clearInterval(identifiantIntervalFlecheRouge);
+        creationFlechBleue();
+        identifiantIntervalFlecheBleue = setInterval(deplacementFlecheBleue,8);
+    }
+    else if (compteur == 5) {
+        document.getElementById('flecheBleue').style.display = 'none';
+        clearInterval(identifiantIntervalFlecheBleue);
+        creationFlechVerte();
+        identifiantIntervalFlecheVerte = setInterval(deplacementFlecheVerte,8);
+    }
+    else if (compteur == 6) {
+        document.getElementById('flecheVerte').style.display = 'none';
+        clearInterval(identifiantIntervalFlecheVerte);
+        mesElementsCibles.boutonIconDIntroduction.src = "Images/Pikachu_Icon_Fermeture.png";
+    }
+    else if (compteur > textesIntroduction.length) {
+        mesElementsCibles.zoneTextDIntroduction.innerHTML = 'À plus tard ! <3';
+        diminutionOpacity(document.getElementById('introduction'));
+        augmentationOpacity(document.getElementById('jeuDesDifferences'));
+    };
+});
+
+
+/* Animation du Pikachu du score */
+document.getElementById('spritePikachu').style.right = "0px";
+setInterval(function(){
+    let x = parseFloat(document.getElementById('spritePikachu').style.right);
+    if (x > 1150) {
+        x = 0;
+        document.getElementById('spritePikachu').style.right = x + 50 + "px";
+    } else {
+        document.getElementById('spritePikachu').style.right = x + 50 + "px";
+    }
+},130);
+
+
+
+
---
+++ jQuery/jQuery_script.js
@@ -0,0 +1,454 @@
+
+
+    /************ Animations de l'intro ************/
+
+$(function(){
+    $(document).ready(function(){
+        $('#introduction').fadeIn(2000)
+    })
+
+})
+    
+
+    /******* Animation : Jeu des 5 différences *******/
+
+
+
+/* Fonction qui anime le déplacement des objets cachés */
+const deplacementObjetsCachés = function (cible,valeurLeft,valeurRight) {
+    cible.animate({
+            left: valeurLeft,
+            top: valeurRight,
+        },2000);
+};
+
+/* animations au click sur les objets cachés */
+$('#differencesCachees>div:nth-child(1)').click(function(){
+    if ($(this).css('left') != '440px') {
+        scoreActuel = augmenteScore();
+        deplacementObjetsCachés($(this),'440px','-85px');
+        balanceTonSwitch();
+    }
+})
+
+$('#differencesCachees>div:nth-child(2)').click(function(){
+    if ($(this).css('left') != '390px') {
+        scoreActuel = augmenteScore();
+        deplacementObjetsCachés($(this),'390px','-50px');
+        balanceTonSwitch();
+    }
+})
+
+$('#differencesCachees>div:nth-child(3)').click(function(){
+    if ($(this).css('left') != '381px') {
+        scoreActuel = augmenteScore();
+        deplacementObjetsCachés($(this),'381px','-81px');
+        balanceTonSwitch();
+    }
+})
+
+$('#differencesCachees>div:nth-child(4)').click(function(){
+    if ($(this).css('left') != '410px') {
+        scoreActuel = augmenteScore();
+        deplacementObjetsCachés($(this),'410px','-105px');
+        balanceTonSwitch();
+    }
+})
+
+$('#differencesCachees>div:nth-child(5)').click(function(){
+    if ($(this).css('left') != '430px') {
+        scoreActuel = augmenteScore();
+        deplacementObjetsCachés($(this),'430px','-55px');
+        balanceTonSwitch();
+    }
+})
+
+
+    /************** Gestion du score et du dévérouillage des balls **************/
+
+
+let scoreActuel;
+let identifiantInterval;
+const $score = $('#score span');
+const $etiquette = $('#etiquettes');
+const $paragrapheEtiquette = $('#etiquettes p');
+const $divJeuDesDifférences = $('#jeuDesDifferences');
+
+/* Gestion du score */
+const augmenteScore = function (){
+    let valeurScore = parseInt($score.html());
+    $score.html(valeurScore + 1);
+    return $score.html();
+};
+
+/* Gestion des étiquettes de jeu */
+const affichageEtiquetteBravo = function (indiceTableau,indiceBall){
+    const textesScore = [
+        'Bravo, tu viens de débloquer la section Formation!',
+        'Bravo, tu viens de débloquer la section Expérience!',
+        'Bravo, tu viens de débloquer la section Compétences!',
+        'Bravo, tu viens de débloquer la section Centres d\'Intérêt!',
+        'Bravo, tu viens de finir le jeux et de débloquer la section Coordonnées!',
+        ]
+    $paragrapheEtiquette.html(textesScore[indiceTableau]);
+    $etiquette.css('display','block');
+    $etiquette.animate({opacity:'1'},500);
+    declenchementDeverrouillage(indiceBall);
+    $divJeuDesDifférences.css('opacity','0.75');
+}
+
+/* Fonction de déclenchement du dévérrouillage */
+const declenchementDeverrouillage = function (indiceBall) {
+    $('#etiquettes img').click(function (){
+        if (indiceBall == 1) {
+            $divJeuDesDifférences.css('opacity','0.75');
+        } else {
+            $divJeuDesDifférences.css('opacity','1');
+        }
+        $etiquette.animate({opacity:'0'},500).fadeOut(1);
+        setTimeout(function(){
+            deverouillageDeBall(indiceBall);
+            indiceBall = 0;
+        },800);
+    })
+}
+
+/* Animation de dévérrouillage */
+const deverouillageDeBall = function (indiceBall) {
+    const $pokeballImage = $('#pokeballCV>div:nth-child(' + indiceBall + ')>img');
+    $pokeballImage.css('bottom','3456px');
+    identifiantInterval = setInterval(function(){
+        let y = parseFloat($pokeballImage.css('bottom'));
+        if (y >= 3968) {
+            clearInterval(identifiantInterval);
+            $pokeballImage.css('bottom','386px');
+            $pokeballImage.css('filter','drop-shadow(2px 2px 5px black)');
+            $pokeballImage.attr('id','activee' + indiceBall);
+        } else {
+            $pokeballImage.css('bottom', y + 128 + 'px');
+        }
+    },120);
+}
+
+
+/* Gestion du dévérouillage selon le score */
+const balanceTonSwitch = function() {
+    switch (parseInt(scoreActuel)) {
+        case 1:
+            affichageEtiquetteBravo(0,5);
+            break;
+        case 2:
+            affichageEtiquetteBravo(1,4);
+            break;
+        case 3:
+            affichageEtiquetteBravo(2,3);
+            break;
+        case 4:
+            affichageEtiquetteBravo(3,2);
+            break;
+        case 5:
+            affichageEtiquetteBravo(4,1);
+            break;
+    }
+}
+
+
+/* Animation d'ouverture des balls*/
+const ouvertureDeBall = function (indiceBall,identifiant) {
+    const $pokeballImage = $('#pokeballCV>div:nth-child(' + indiceBall + ')>img');
+    if ($pokeballImage.attr('id') == ('activee' + indiceBall)) {
+        $pokeballImage.attr('id','enCours')
+        identifiant = setInterval(function(){
+            let y = parseFloat($pokeballImage.css('bottom'));
+            if (y >= 1792) {
+                clearInterval(identifiant);
+                $pokeballImage.css('bottom','386px');
+                $pokeballImage.attr('id','ouverte' + indiceBall);
+            } else {
+                $pokeballImage.css('bottom', y + 128 + 'px');
+            }
+        },120);
+    }
+}
+
+/* Animation de fermeture des balls*/
+const fermetureDeBall = function (indiceBall,identifiant) {
+    const $pokeballImage = $('#pokeballCV>div:nth-child(' + indiceBall + ')>img');
+    if ($pokeballImage.attr('id') == ('ouverte' + indiceBall)) {
+        $pokeballImage.attr('id','enCours')
+        $pokeballImage.css('bottom','3330px');
+        identifiant = setInterval(function(){
+            let y = parseFloat($pokeballImage.css('bottom'));
+            if (y <= 1922) {
+                $pokeballImage.css('bottom', y + 128 + 'px');
+                clearInterval(identifiant);
+                setTimeout(function(){
+                    $pokeballImage.css('bottom','386px');
+                },120)
+                $pokeballImage.attr('id','activee' + indiceBall);
+            } else {
+                $pokeballImage.css('bottom', y - 128 + 'px');
+            }
+        },120);
+    }
+}
+
+
+
+    /****** Gère les ouvertures/fermetures des pokeballCV ******/
+
+
+for (let i=0; i < 6 ; i++) {
+    const $pokeball = $('#pokeballCV>div:nth-child(' + i + ')>img');
+    const $sectionCV = $('#pokeCV>section:nth-child(' + i + ')');
+    $pokeball.click(function (){
+        if ($pokeball.attr('id') == ('activee' + i)) {
+            ouvertureDeBall(i,identifiantInterval);
+            $sectionCV.css('display','block');
+            switch (i) {
+                case 1:
+                    ouvertureAquali();
+                    break;
+                case 2:
+                    ouvertureVoltali();
+                    break;
+                case 3:
+                    ouvertureNoctali();
+                    break;
+                case 4:
+                    ouverturePyroli();
+                    break;
+                case 5:
+                    ouvertureMentali();
+                    break;
+            }
+            $sectionCV.animate({
+                right: calculMilieuLargeur,
+                bottom: calculMilieuHauteur,
+            },1000)
+        } 
+        if ($pokeball.attr('id') == ('ouverte' + i)) {
+            switch (i) {
+                case 1:
+                    fermetureAquali();
+                    break;
+                case 2:
+                    fermetureVoltali();
+                    break;
+                case 3:
+                    fermetureNoctali();
+                    break;
+                case 4:
+                    fermeturePyroli();
+                    break;
+                case 5:
+                    fermetureMentali();
+                    break;
+            }
+            $sectionCV.animate({
+                right: '0px',
+                bottom: '0px',
+            },1000)
+            $sectionCV.delay(1000).fadeOut(1)
+            fermetureDeBall(i,identifiantInterval);
+        }
+    })
+}
+
+/* Fonctions de déplacement des étiquette à l'OUVERTURE des balls */
+const ouvertureAquali = function (){
+    $('#pokeCV>section:nth-child(1)>div').animate({
+        bottom: '121px',
+        opacity: '1',
+        right: '31px',
+    },1000)
+    $('#pokeCV>section:nth-child(1)>div>div').animate({
+        height: '220px',
+        width: '370px',
+    },1000)
+    $('#pokeCV>section:nth-child(1) img').animate({
+        bottom: '-8px',
+        right: '-55px',
+        width: '185px',
+    },1000)
+    $('#pokeCV>section:nth-child(1) h2,#pokeCV>section:nth-child(1) address').delay(800).fadeIn(1000)
+}
+const ouvertureVoltali = function (){
+    $('#pokeCV>section:nth-child(2)>div').animate({
+        bottom: '-14px',
+        opacity: '1',
+        right: '31px',
+    },1000)
+    $('#pokeCV>section:nth-child(2)>div>div').animate({
+        height: '490px',
+        width: '370px',
+    },1000)
+    $('#pokeCV>section:nth-child(2) img').animate({
+        bottom: '-8px',
+        right: '-66px',
+        width: '185px',
+    },1000)
+    $('#pokeCV>section:nth-child(2) h2,#pokeCV>section:nth-child(2) h3,#pokeCV>section:nth-child(2) ul').delay(800).fadeIn(1000)
+}
+const ouvertureNoctali = function () {
+    $('#pokeCV>section:nth-child(3)>div').animate({
+        bottom: '94px',
+        opacity: '1',
+        right: '31px',
+    },1000)
+    $('#pokeCV>section:nth-child(3)>div>div').animate({
+        height: '275px',
+        width: '370px',
+    },1000)
+    $('#pokeCV>section:nth-child(3) img').animate({
+        bottom: '-14px',
+        right: '325px',
+        width: '185px',
+    },1000)
+    $('#pokeCV>section:nth-child(3) h2,#pokeCV>section:nth-child(3) ul').delay(800).fadeIn(1000)
+}
+const ouverturePyroli = function (){
+    $('#pokeCV>section:nth-child(4)>div').animate({
+        bottom: '11px',
+        opacity: '1',
+        right: '-49px',
+    },1000)
+    $('#pokeCV>section:nth-child(4)>div>div').animate({
+        height: '440px',
+        width: '530px',
+    },1000)
+    $('#pokeCV>section:nth-child(4) img').animate({
+        bottom: '370px',
+        right: '-25px',
+        width: '185px',
+    },1000)
+    $('#pokeCV>section:nth-child(4) h2,#pokeCV>section:nth-child(4) h3,#pokeCV>section:nth-child(4) h4,#pokeCV>section:nth-child(4) p').delay(800).fadeIn(1000)
+}
+const ouvertureMentali = function (){
+    $('#pokeCV>section:nth-child(5)>div').animate({
+        bottom: '-55px',
+        opacity: '1',
+        right: '-101px',
+    },1000)
+    $('#pokeCV>section:nth-child(5)>div>div').animate({
+        height: '565px',
+        width: '635px',
+    },1000)
+    $('#pokeCV>section:nth-child(5) img').animate({
+        bottom: '470px',
+        right: '10px',
+        width: '185px',
+    },1000)
+    $('#pokeCV>section:nth-child(5) h2,#pokeCV>section:nth-child(5) h3,#pokeCV>section:nth-child(5) h4,#pokeCV>section:nth-child(5) address,#pokeCV>section:nth-child(5) p').delay(800).fadeIn(1000)
+}
+
+/* Fonctions de déplacement des étiquette à la FERMETURE des balls */
+const fermetureAquali = function (){
+    $('#pokeCV>section:nth-child(1)>div').animate({
+        bottom: '378px',
+        opacity: '0',
+        right: '40px',
+    },1000)
+    $('#pokeCV>section:nth-child(1)>div>div').animate({
+        height: '0px',
+        width: '0px',
+    },1000)
+    $('#pokeCV>section:nth-child(1) img').animate({
+        bottom: '9px',
+        right: '27px',
+        width: '20px',
+    },1000)
+    $('#pokeCV>section:nth-child(1) h2,#pokeCV>section:nth-child(1) address').fadeOut(500)
+}
+const fermetureVoltali = function (){
+    $('#pokeCV>section:nth-child(2)>div').animate({
+        bottom: '297px',
+        opacity: '0',
+        right: '40px',
+    },1000)
+    $('#pokeCV>section:nth-child(2)>div>div').animate({
+        height: '0px',
+        width: '0px',
+    },1000)
+    $('#pokeCV>section:nth-child(2) img').animate({
+        bottom: '11px',
+        right: '26px',
+        width: '20px',
+    },1000)
+    $('#pokeCV>section:nth-child(2) h2,#pokeCV>section:nth-child(2) h3,#pokeCV>section:nth-child(2) ul').fadeOut(500)
+}
+const fermetureNoctali = function (){
+    $('#pokeCV>section:nth-child(3)>div').animate({
+        bottom: '218px',
+        opacity: '0',
+        right: '40px',
+    },1000)
+    $('#pokeCV>section:nth-child(3)>div>div').animate({
+        height: '0px',
+        width: '0px',
+    },1000)
+    $('#pokeCV>section:nth-child(3) img').animate({
+        bottom: '9px',
+        right: '25px',
+        width: '20px',
+    },1000)
+    $('#pokeCV>section:nth-child(3) h2,#pokeCV>section:nth-child(3) ul').fadeOut(500)
+}
+const fermeturePyroli = function (){
+    $('#pokeCV>section:nth-child(4)>div').animate({
+        bottom: '137px',
+        opacity: '0',
+        right: '40px',
+    },1000)
+    $('#pokeCV>section:nth-child(4)>div>div').animate({
+        height: '0px',
+        width: '0px',
+    },1000)
+    $('#pokeCV>section:nth-child(4) img').animate({
+        bottom: '11px',
+        right: '25px',
+        width: '20px',
+    },1000)
+    $('#pokeCV>section:nth-child(4) h2,#pokeCV>section:nth-child(4) h3,#pokeCV>section:nth-child(4) h4,#pokeCV>section:nth-child(4) p').fadeOut(500)
+}
+const fermetureMentali = function (){
+    $('#pokeCV>section:nth-child(5)>div').animate({
+        bottom: '58px',
+        opacity: '0',
+        right: '40px',
+    },1000)
+    $('#pokeCV>section:nth-child(5)>div>div').animate({
+        height: '0px',
+        width: '0px',
+    },1000)
+    $('#pokeCV>section:nth-child(5) img').animate({
+        bottom: '11px',
+        right: '27px',
+        width: '20px',
+    },1000)
+    $('#pokeCV>section:nth-child(5) h2,#pokeCV>section:nth-child(5) h3,#pokeCV>section:nth-child(5) h4,#pokeCV>section:nth-child(5) address,#pokeCV>section:nth-child(5) p').fadeOut(500)
+}
+
+
+/*** Fonction pour gérer le centrage des étiquettes du CV ***/
+let valeurLargeur = parseFloat($('#pokeCV').css('width'));
+let valeurHauteur = parseFloat($('#pokeCV').css('height'));
+let calculMilieuLargeur = ((valeurLargeur/2) - 250) + "px";
+let calculMilieuHauteur = ((valeurHauteur/2) - 250) + "px";
+
+$(window).resize(function(){
+    valeurLargeur = parseFloat($('#pokeCV').css('width'));
+    valeurHauteur = parseFloat($('#pokeCV').css('height'));
+    calculMilieuLargeur = ((valeurLargeur/2) - 250) + "px";
+    calculMilieuHauteur = ((valeurHauteur/2) - 250) + "px";
+    for (let i=0; i < 6 ; i++) {
+        const $sectionCV = $('#pokeCV>section:nth-child(' + i + ')');
+        if ($sectionCV.css('display') != 'none') {
+            $sectionCV.css({
+                right: calculMilieuLargeur,
+                bottom: calculMilieuHauteur,
+            })
+        }
+    }
+    
+})
